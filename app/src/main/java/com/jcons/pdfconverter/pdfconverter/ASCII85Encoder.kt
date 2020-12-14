a887cfb4d4851984ba8a0d894377996bc1fd4b68
André Lessa Guedes <aguedes.rj@hotmail.com>
2020-12-14 18:40:41 -0300

    Converter Java pra Kotlin: ASCII85Encoder e XObjectImage

---
+++ app/src/main/java/com/jcons/pdfconverter/pdfconverter/ASCII85Encoder.kt
@@ -0,0 +1,222 @@
+package com.jcons.pdfconverter.pdfconverter
+
+import java.io.FilterOutputStream
+import java.io.IOException
+import java.io.OutputStream
+
+/**
+ * This class encodes a binary stream into a text stream.
+ *
+ *
+ *
+ * The ASCII85encoding is suitable when binary data needs to be transmitted or stored as text. It has been defined by
+ * Adobe for the PostScript and PDF formats (see PDF Reference, section 3.3 Details of Filtered Streams).
+ *
+ *
+ *
+ *
+ * The encoded stream is about 25% larger than the corresponding binary stream (32 binary bits are converted into 40
+ * encoded bits, and there may be start/end of line markers).
+ *
+ *
+ */
+class ASCII85Encoder : FilterOutputStream {
+    /**
+     * Create an encoder wrapping a sink of binary data.
+     *
+     *
+     * Calling this constructor is equivalent to calling [ ASCII85Encoder(&lt;code&gt;out&lt;/code&gt;, &lt;code&gt;-1&lt;/code&gt;, &lt;code&gt;null&lt;/code&gt;, &lt;code&gt;null&lt;/code&gt;)][.ASCII85Encoder].
+     *
+     *
+     * @param out sink of binary data to filter
+     */
+    constructor(out: OutputStream?) : super(out) {
+        lineLength = -1
+        c1 = -1
+        phase = 4
+    }
+
+    /**
+     * Create an encoder wrapping a sink of binary data.
+     *
+     *
+     * The additional arguments allow to specify some text formatting
+     *
+     *
+     *
+     * Note that specifying a negative number for `lineLength` is really equivalent to calling the one
+     * argument [constructor][.ASCII85Encoder].
+     *
+     *
+     *
+     * If non-null start/end of line are used, they must contain only whitespace characters as other characters would
+     * otherwise interfere with the decoding process on the other side of the channel. For safety, it is recommended to
+     * stick to space (' ', 0x32) and horizontal tabulation ('\t', 0x9) characters for the start of line marker, and to
+     * line feed ('\n', 0xa) and carriage return ('\r', 0xd) characters according to the platform convention for the end
+     * of line marker.
+     *
+     *
+     * @param out        sink of binary data to filter
+     * @param lineLength maximal length of a ligne (counting `sol` but not counting `eol`), if
+     * negative lines will not be split
+     * @param sol        start of line marker to use (mainly for indentation purposes), may be null
+     * @param eol        end of line marker to use, may be null only if `lineLength` is negative
+     */
+    constructor(out: OutputStream?, lineLength: Int, sol: ByteArray?, eol: ByteArray) : super(out) {
+        this.lineLength = lineLength
+        this.sol = sol
+        this.eol = eol
+        c1 = -1
+        phase = 4
+    }
+
+    /**
+     * Closes this output stream and releases any system resources associated with the stream.
+     *
+     * @throws IOException if the underlying stream throws one
+     */
+    @Throws(IOException::class)
+    override fun close() {
+        if (c1 >= 0) {
+            c4 += c5 / 85
+            c3 += c4 / 85
+            c2 += c3 / 85
+            c1 += c2 / 85
+
+            // output only the required number of bytes
+            putByte(33 + c1)
+            putByte(33 + c2 % 85)
+            if (phase > 1) {
+                putByte(33 + c3 % 85)
+                if (phase > 2) {
+                    putByte(33 + c4 % 85)
+                    if (phase > 3) {
+                        putByte(33 + c5 % 85)
+                    }
+                }
+            }
+
+            // output the end marker
+            putByte('~'.toInt())
+            putByte('>'.toInt())
+        }
+
+        // end the last line properly
+        if (length != 0) {
+            out.write(eol, 0, eol.size)
+        }
+
+        // close the underlying stream
+        out.close()
+    }
+
+    /**
+     * Writes the specified byte to this output stream.
+     *
+     * @param b byte to write (only the 8 low order bits are used)
+     */
+    @Throws(IOException::class)
+    override fun write(b: Int) {
+        var b = b
+        b = b and 0xff
+        when (phase) {
+            1 -> {
+                c3 += 9 * b
+                c4 += 6 * b
+                c5 += b
+                phase = 2
+            }
+            2 -> {
+                c4 += 3 * b
+                c5 += b
+                phase = 3
+            }
+            3 -> {
+                c5 += b
+                phase = 4
+            }
+            else -> {
+                if (c1 >= 0) {
+                    // there was a preceding quantum, we now know it was not the last
+                    if (c1 == 0 && c2 == 0 && c3 == 0 && c4 == 0 && c5 == 0) {
+                        putByte('z'.toInt())
+                    } else {
+                        c4 += c5 / 85
+                        c3 += c4 / 85
+                        c2 += c3 / 85
+                        c1 += c2 / 85
+                        putByte(33 + c1)
+                        putByte(33 + c2 % 85)
+                        putByte(33 + c3 % 85)
+                        putByte(33 + c4 % 85)
+                        putByte(33 + c5 % 85)
+                    }
+                }
+                c1 = 0
+                c2 = 27 * b
+                c3 = c2
+                c4 = 9 * b
+                c5 = b
+                phase = 1
+            }
+        }
+    }
+
+    /**
+     * Put a byte in the underlying stream, inserting line breaks as needed.
+     *
+     * @param b byte to put in the underlying stream (only the 8 low order bits are used)
+     * @throws IOException if the underlying stream throws one
+     */
+    @Throws(IOException::class)
+    private fun putByte(b: Int) {
+        if (lineLength >= 0) {
+            // split encoded lines if needed
+            if (length == 0 && sol != null) {
+                out.write(sol, 0, sol!!.size)
+                length = sol!!.size
+            }
+            out.write(b)
+            if (++length >= lineLength) {
+                out.write(eol, 0, eol.size)
+                length = 0
+            }
+        } else {
+            out.write(b)
+        }
+    }
+
+    /**
+     * Line length (not counting eol).
+     */
+    private var lineLength: Int
+
+    /**
+     * Start of line marker (indentation).
+     */
+    private var sol: ByteArray?
+
+    /**
+     * End Of Line marker.
+     */
+    private var eol: ByteArray
+
+    /**
+     * Coefficients of the 32-bits quantum in base 85.
+     */
+    private var c1: Int
+    private var c2 = 0
+    private var c3 = 0
+    private var c4 = 0
+    private var c5 = 0
+
+    /**
+     * Phase (between 1 and 4) of raw bytes.
+     */
+    private var phase: Int
+
+    /**
+     * Current length of the line being written.
+     */
+    private var length = 0
+}
---
+++ app/src/main/java/com/jcons/pdfconverter/pdfconverter/XObjectImage.kt
@@ -0,0 +1,137 @@
+package com.jcons.pdfconverter.pdfconverter
+
+import com.jcons.pdfconverter.pdfconverter.PDFDocument.newIndirectObject
+import com.jcons.pdfconverter.pdfconverter.PDFDocument.includeIndirectObject
+import com.jcons.pdfconverter.pdfconverter.IndirectObject.addDictionaryContent
+import com.jcons.pdfconverter.pdfconverter.IndirectObject.addStreamContent
+import com.jcons.pdfconverter.pdfconverter.IndirectObject.indirectReference
+import com.jcons.pdfconverter.pdfconverter.PDFDocument
+import android.graphics.Bitmap
+import com.jcons.pdfconverter.pdfconverter.IndirectObject
+import com.jcons.pdfconverter.pdfconverter.XObjectImage
+import com.jcons.pdfconverter.pdfconverter.ASCII85Encoder
+import com.jcons.pdfconverter.pdfconverter.Indentifiers
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.util.zip.Deflater
+import java.util.zip.DeflaterOutputStream
+
+class XObjectImage(private val mDocument: PDFDocument, bitmap: Bitmap) {
+    private var mIndirectObject: IndirectObject? = null
+    private var mDataSize = 0
+    var width = -1
+        private set
+    var height = -1
+        private set
+    var name = ""
+    var id = ""
+    private var mProcessedImage: String? = ""
+    fun appendToDocument() {
+        mIndirectObject = mDocument.newIndirectObject()
+        mDocument.includeIndirectObject(mIndirectObject)
+        mIndirectObject!!.addDictionaryContent(
+            """ /Type /XObject
+ /Subtype /Image
+ /Filter [/ASCII85Decode /FlateDecode]
+ /Width ${width}
+ /Height ${height}
+ /BitsPerComponent ${Integer.toString(BITSPER_COMPONENT)}
+ /Interpolate ${java.lang.Boolean.toString(INTERPOLATION)}
+ /ColorSpace $DEVICE_RGB
+ /Length ${mProcessedImage!!.length}
+"""
+        )
+        mIndirectObject!!.addStreamContent(mProcessedImage)
+    }
+
+    private fun configureBitmap(bitmap: Bitmap): Bitmap? {
+        val img = bitmap.copy(Bitmap.Config.ARGB_8888, false)
+        if (img != null) {
+            width = img.width
+            height = img.height
+            mDataSize = width * height * 3
+        }
+        return img
+    }
+
+    private fun getBitmapData(bitmap: Bitmap?): ByteArray? {
+        var data: ByteArray? = null
+        if (bitmap != null) {
+            data = ByteArray(mDataSize)
+            var intColor: Int
+            var offset = 0
+            for (y in 0 until height) {
+                for (x in 0 until width) {
+                    intColor = bitmap.getPixel(x, y)
+                    data[offset++] = (intColor shr 16 and 0xFF).toByte()
+                    data[offset++] = (intColor shr 8 and 0xFF).toByte()
+                    data[offset++] = (intColor shr 0 and 0xFF).toByte()
+                }
+            }
+        }
+        return data
+    }
+
+    private fun deflateImageData(baos: ByteArrayOutputStream, data: ByteArray?): Boolean {
+        if (data != null) {
+            val deflater = Deflater(COMPRESSION_LEVEL)
+            val dos = DeflaterOutputStream(baos, deflater)
+            try {
+                dos.write(data)
+                dos.close()
+                deflater.end()
+                return true
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+        return false
+    }
+
+    private fun encodeImageData(baos: ByteArrayOutputStream): String {
+        val sob = ByteArrayOutputStream()
+        val enc85 = ASCII85Encoder(sob)
+        try {
+            var i = 0
+            for (b in baos.toByteArray()) {
+                enc85.write(b.toInt())
+                if (i++ == 255) {
+                    sob.write('\n'.toInt())
+                    i = 0
+                }
+            }
+            return sob.toString(ENCODING)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return ""
+    }
+
+    private fun processImage(bitmap: Bitmap?): String? {
+        val baos = ByteArrayOutputStream()
+        return if (deflateImageData(baos, getBitmapData(bitmap))) {
+            encodeImageData(baos)
+        } else null
+    }
+
+    fun asXObjectReference(): String {
+        return name + " " + mIndirectObject!!.indirectReference
+    }
+
+    companion object {
+        const val BITSPERCOMPONENT_8 = 8
+        const val DEVICE_RGB = "/DeviceRGB"
+        const val INTERPOLATION = false
+        const val BITSPER_COMPONENT = BITSPERCOMPONENT_8
+        const val COLOR_SPACE = DEVICE_RGB
+        const val COMPRESSION_LEVEL = Deflater.NO_COMPRESSION
+        const val ENCODING = "ISO-8859-1"
+        var mImageCount = 0
+    }
+
+    init {
+        mProcessedImage = processImage(configureBitmap(bitmap))
+        id = Indentifiers.generateId(mProcessedImage)
+        name = "/img" + ++mImageCount
+    }
+}
