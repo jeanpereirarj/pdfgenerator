a83110cc02990c6b34486eabf2d2e531e73cd584
Carlos Jean <c_jean@outlook.com>
2020-12-17 10:25:45 -0300

    commit inicial

--- app/src/main/java/com/jcons/pdfconverter/Compress.kt
+++ app/src/main/java/com/jcons/pdfconverter/Compress.kt
@@ -2,7 +2,11 @@ package com.jcons.pdfconverter
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
 import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 
 object Compress {
@@ -51,3 +55,89 @@ object Compress {
 
 private const val DEFAULT_WIDTH: Int = 612
 private const val DEFAULT_HEIGTH: Int = 816
+
+
+fun compressorFile(imageFile: File, quality: Int){
+    decodeSampledBitmapFromFile(imageFile, DEFAULT_WIDTH, DEFAULT_HEIGTH).run {
+        overWrite(imageFile, this, imageFile.compressFormat(), quality)
+    }
+}
+
+private fun decodeSampledBitmapFromFile(imageFile: File, reqWidth: Int, reqHeight: Int): Bitmap {
+    return BitmapFactory.Options().run {
+        inJustDecodeBounds = true
+        BitmapFactory.decodeFile(imageFile.absolutePath, this)
+
+        inSampleSize = calculateInSampleSize(this, reqWidth, reqHeight)
+
+        inJustDecodeBounds = false
+        BitmapFactory.decodeFile(imageFile.absolutePath, this)
+    }
+}
+
+private fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
+
+    val (height: Int, width: Int) = options.run { outHeight to outWidth }
+    var inSampleSize = 1
+
+    if (height > reqHeight || width > reqWidth) {
+
+        val halfHeight: Int = height / 2
+        val halfWidth: Int = width / 2
+
+        while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
+            inSampleSize *= 2
+        }
+    }
+
+    return inSampleSize
+}
+
+fun overWrite(imageFile: File, bitmap: Bitmap, format: Bitmap.CompressFormat = imageFile.compressFormat(), quality: Int = 100): File {
+    val result = if (format == imageFile.compressFormat()) {
+        imageFile
+    } else {
+        File("${imageFile.absolutePath.substringBeforeLast(".")}.${format.extension()}")
+    }
+    imageFile.delete()
+    saveBitmap(bitmap, result, format, quality)
+    return result
+}
+
+private fun determineImageRotation(imageFile: File, bitmap: Bitmap): Bitmap {
+    val exif = ExifInterface(imageFile.absolutePath)
+    val orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 0)
+    val matrix = Matrix()
+    when (orientation) {
+        6 -> matrix.postRotate(90f)
+        3 -> matrix.postRotate(180f)
+        8 -> matrix.postRotate(270f)
+    }
+    return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+}
+
+fun File.compressFormat() = when (extension.toLowerCase()) {
+    "png" -> Bitmap.CompressFormat.PNG
+    "webp" -> Bitmap.CompressFormat.WEBP
+    else -> Bitmap.CompressFormat.JPEG
+}
+
+fun Bitmap.CompressFormat.extension() = when (this) {
+    Bitmap.CompressFormat.PNG -> "png"
+    Bitmap.CompressFormat.WEBP -> "webp"
+    else -> "jpg"
+}
+
+fun saveBitmap(bitmap: Bitmap, destination: File, format: Bitmap.CompressFormat = destination.compressFormat(), quality: Int = 100) {
+    destination.parentFile?.mkdirs()
+    var fileOutputStream: FileOutputStream? = null
+    try {
+        fileOutputStream = FileOutputStream(destination.absolutePath)
+        bitmap.compress(format, quality, fileOutputStream)
+    } finally {
+        fileOutputStream?.run {
+            flush()
+            close()
+        }
+    }
+}
